58f969cdfbd560b90b736c891226be6d91c20968
Victor Nunes <victor.nuness787@gmail.com>
2025-02-22 22:18:25 -0300

    banner e imagens novas, e organização

--- landigpage/produtos.js
+++ landigpage/produtos.js
@@ -2,73 +2,121 @@ const produtos = [
     { 
       nome: "Lenço Umedecido", 
       preco: 8, 
-      imagem: "imagens/lenço.jpg", 
+      imagem: "imagens/produtos/lenço.jpeg", 
       descricao: "Lenço umedecido para limpeza facial, com ingredientes suaves e ideais para pele sensível." 
     },
     { 
       nome: "Esponja Para Base", 
       preco: 4, 
-      imagem: "imagens/produto1.jpeg", 
+      imagem: "imagens/produtos/produto1.jpeg", 
       descricao: "Esponja macia para aplicação de base líquida, garantindo um acabamento natural." 
     },
     { 
       nome: "Rímel", 
       preco: 10, 
-      imagem: "imagens/produto2.jpg", 
+      imagem: "imagens/produtos/produto2.jpeg", 
       descricao: "Máscara de cílios resistente à água que realça seus olhos com volume e definição." 
     },
     { 
       nome: "Água Micelar", 
       preco: 12, 
-      imagem: "imagens/aguamicelar.jpg", 
+      imagem: "imagens/produtos/aguamicelar.jpeg", 
       descricao: "Água micelar suave que remove impurezas e maquiagem sem agredir a pele." 
     },
     { 
       nome: "Batom", 
       preco: 15, 
-      imagem: "imagens/batom.png", 
+      imagem: "imagens/produtos/batom.jpeg", 
       descricao: "Batom de longa duração com cores vibrantes e acabamento aveludado." 
     },
     { 
       nome: "Sabonete", 
       preco: 15, 
-      imagem: "imagens/sabonete.jpeg", 
+      imagem: "imagens/produtos/sabonete.jpeg", 
       descricao: "Sabonete hidratante para limpeza facial, enriquecido com extratos naturais." 
     },
     { 
       nome: "Sérum", 
       preco: 15, 
-      imagem: "imagens/serum.jpeg", 
+      imagem: "imagens/produtos/serumesfoliante.jpeg", 
       descricao: "Sérum antioxidante que ajuda a iluminar e proteger a pele dos radicais livres." 
     },
     { 
       nome: "Gloss", 
       preco: 12, 
-      imagem: "imagens/gloss.jpeg", 
+      imagem: "imagens/produtos/gloss.jpeg", 
       descricao: "Gloss brilhante que proporciona um toque sedoso e um efeito iluminado aos lábios." 
     },
     { 
       nome: "Mascara Facial", 
       preco: 2, 
-      imagem: "imagens/mascarafacil.jpeg", 
+      imagem: "imagens/produtos/mascarafacil.jpeg", 
       descricao: "Máscara facial para suavizar e revitalizar a pele, ideal para uma rotina de beleza." 
     },
     { 
-      nome: "Pó Compacto", 
+      nome: "Iluminador", 
       preco: 15, 
-      imagem: "imagens/po.jpeg", 
+      imagem: "imagens/produtos/iluminador.jpeg", 
       descricao: "Pó compacto de alta cobertura para finalizar a maquiagem com um acabamento impecável." 
     },
     { 
       nome: "Lápis de Olho", 
       preco: 12, 
-      imagem: "imagens/lapis.jpeg", 
+      imagem: "imagens/produtos/lapis.jpeg", 
       descricao: "Lápis de olho de alta definição para realçar e contornar os olhos com precisão." 
     },
     { 
       nome: "Delineador", 
-      preco: 2, 
-      imagem: "imagens/delineador.jpg", 
+      preco: 10, 
+      imagem: "imagens/produtos/delineador.jpeg", 
+      descricao: "Delineador prático e preciso para um look marcante e bem definido." 
+    },
+    { 
+      nome: "Escova de Silicone", 
+      preco: 4, 
+      imagem: "imagens/produtos/esponjapo.jpeg", 
+      descricao: "Delineador prático e preciso para um look marcante e bem definido." 
+    },
+    { 
+      nome: "Esponja para Pó", 
+      preco: 12, 
+      imagem: "imagens/produtos/escovasilicone.jpeg", 
+      descricao: "Delineador prático e preciso para um look marcante e bem definido." 
+    },
+    { 
+      nome: "Apontador", 
+      preco: 10, 
+      imagem: "imagens/produtos/apontador.jpeg", 
+      descricao: "Delineador prático e preciso para um look marcante e bem definido." 
+    },
+    { 
+      nome: "Blindagem", 
+      preco: 14, 
+      imagem: "imagens/produtos/blindagem.jpeg", 
+      descricao: "Delineador prático e preciso para um look marcante e bem definido." 
+    },
+    { 
+      nome: "Paleta de Sombra", 
+      preco: 20, 
+      imagem: "imagens/produtos/paletasombra.jpeg", 
+      descricao: "Delineador prático e preciso para um look marcante e bem definido." 
+    },
+    { 
+      nome: "Primer Facil", 
+      preco: 16, 
+      imagem: "imagens/produtos/primerfacil.jpeg", 
+      descricao: "Delineador prático e preciso para um look marcante e bem definido." 
+    },
+    { 
+      nome: "Primer Facil", 
+      preco: 16, 
+      imagem: "imagens/produtos/primerfacil.jpeg", 
+      descricao: "Delineador prático e preciso para um look marcante e bem definido." 
+    },
+    { 
+      nome: "Serum Clareador", 
+      preco: 14, 
+      imagem: "imagens/produtos/serumclareador.jpeg", 
       descricao: "Delineador prático e preciso para um look marcante e bem definido." 
     }
   ];
